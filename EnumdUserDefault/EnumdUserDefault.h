a693f14b0ca582574c00eeb1df50e45f8396c547
Hideki Matsuoka <matsuoka.hide@gmail.com>
2017-10-01 17:40:43 +0900

    Initial Commit

---
+++ EnumdUserDefault/EnumdUserDefault.h
@@ -0,0 +1,19 @@
+//
+//  EnumdUserDefault.h
+//  EnumdUserDefault
+//
+//  Created by matsuokah on 2017/10/01.
+//  Copyright © 2017 matsuokah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EnumdUserDefault.
+FOUNDATION_EXPORT double EnumdUserDefaultVersionNumber;
+
+//! Project version string for EnumdUserDefault.
+FOUNDATION_EXPORT const unsigned char EnumdUserDefaultVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnumdUserDefault/PublicHeader.h>
+
+
